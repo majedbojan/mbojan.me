0318c056ef94682b35cde54465d4f91c08bef162
MaJeD BoJaN <bojanmajed@gmail.com>
2020-09-14 18:50:16 +0300

    working on mailchimp integration

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+// import MyGatsbyComponent from "../components/MailchimpForm"
 import { rhythm } from "../utils/typography"
 
 const BlogIndex = ({ data, location }) => {
@@ -14,6 +15,7 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
+      {/* <MyGatsbyComponent /> */}
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
